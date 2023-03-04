85da1bfc81a5166e84fe3eb72454cab11ec3cc67
arohao <119353048+arohao@users.noreply.github.com>
2023-03-04 15:55:01 -0500

    CONNECTED TO SOCKETIO

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const fs = require('fs');
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.static(__dirname));
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+  });
+
+
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
